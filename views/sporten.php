61aa00e13b5e7033e134c8b093586a770ef5eba9
joey1006 <joeysteffens@quicknet.nl>
2016-02-24 14:12:34 +0100

    query start sporten.php

--- views/sporten.php
+++ views/sporten.php
@@ -1,1 +1,22 @@
 <h1>random shit</h1>
+
+
+
+<?php
+
+$sql = "SELECT * FROM clubkit_sports";
+$result = $mysqli->query($sql);
+while($row = mysqli_fetch_assoc($result)) {
+    $sports[] = $row;
+
+    echo $sports['name'];
+//    echo "<img src=".$row['picture'].">";
+}
+
+
+
+//$sql = "SELECT * FROM products";
+//$resultproducts = $mysqli->query($sql);
+//$resultproducts = resultToArray($resultproducts)
+
+?>
